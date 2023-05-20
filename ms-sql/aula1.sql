3f14969eec7cc8a7d05ab493d4bf4ca1a86db4f0
JOÃO PEDRO MOTA MELÁRA <3262.343@edu.fb.org.br>
2023-05-19 21:52:49 -0300

    PRIMEIRO EXEMPLO DE COMANDOS SQL

---
+++ ms-sql/aula1.sql
@@ -0,0 +1,10 @@
+SELECT * FROM HumanResources.Department
+
+SELECT * FROM SYS.tables
+
+SELECT FirstName, LastName FROM Person.Person
+
+SELECT FirstName AS PrimeiroNome, 
+	   LastName AS UltimoNome 
+FROM Person.Person
+
