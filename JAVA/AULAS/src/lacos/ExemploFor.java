09066f8d411f2e7f3cff2de846e640fe3fc04c2b
JOÃO PEDRO MOTA MELÁRA <3262.343@edu.fb.org.br>
2023-06-27 19:42:43 -0300

    lacos

---
+++ JAVA/AULAS/src/lacos/ExemploFor.java
@@ -0,0 +1,29 @@
+package lacos;
+
+import java.util.Scanner;
+public class ExemploFor {
+	public static void main (String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+	char op = ' ';	
+		
+	do {
+		for(int i=1; i<=60; i++) {
+			System.out.print("-");	
+		}
+		System.out.println();
+		System.out.print("Continua S/N");
+		op = input.next().toUpperCase().charAt(0);
+	}while(op!='N');
+	
+	while(true) {
+		System.out.println("Fala sério!");
+		System.out.println("Continua S/N :");
+		op=input.next().toUpperCase().charAt(0);
+		if(op=='N') {
+			break;
+		}
+	}
+
+	}
+}
