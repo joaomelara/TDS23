610507db9c4e625dfcda20a403290ab23045dd55
JOÃO PEDRO MOTA MELÁRA <3262.343@edu.fb.org.br>
2023-06-26 21:51:36 -0300

    aula

---
+++ JAVA/AULAS/src/parImpar.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class parImpar {
+	public static void main(String[]args) {
+		Scanner input = new Scanner(System.in);
+	
+			int num;
+			System.out.println("Fala um número ae:");
+			num = input.nextInt();
+			
+			if(num>=0) {
+				if((num%2) == 0)
+				{
+					System.out.println("O número digitado é par!");
+				}
+				else
+				{
+					System.out.println("O número digitado é impar!");
+				}
+			}
+			else
+			{
+				System.out.println("Eu disee positivo bobão!");
+			}
+			input.close();
+			
+	}
+}
