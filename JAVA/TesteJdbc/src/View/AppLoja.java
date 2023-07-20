15ec07d13be42b2daf95b233a6b63985b2f517b4
JOÃO PEDRO MOTA MELÁRA <3262.343@edu.fb.org.br>
2023-07-19 21:55:35 -0300

    spring

---
+++ JAVA/TesteJdbc/src/View/AppLoja.java
@@ -0,0 +1,15 @@
+package View;
+
+import dao.ProdutoDao;
+import model.Produto;
+
+public class AppLoja {
+	public static void main(String[] args) {
+		
+		ProdutoDao dao = new ProdutoDao();
+		
+		for(Produto p : dao.listar()) {
+			System.out.println(p.toString());
+		}
+	}
+}
